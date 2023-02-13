5a46c322353d7ed5be584d2a0a45989759a7e750
landon tomkins <landontomkins@gmail.com>
2023-02-13 15:53:30 -0600

    fixed node.js API

--- Capstone_Webpage/sql/parkingLots.sql
+++ Capstone_Webpage/sql/parkingLots.sql
@@ -7,5 +7,21 @@ CREATE TABLE parkingLots (
     spaceID VARCHAR(255) NOT NULL,
     type    VARCHAR(255) NOT NULL,
     availability INTEGER NOT NULL,
-    PRIMARY KEY (lot_name)
+    PRIMARY KEY (spaceID)
+);
+
+INSERT INTO parkingLots
+VALUES(
+    "Nethkin",
+    "6",
+    "student",
+    1
+);
+
+INSERT INTO parkingLots
+VALUES(
+    "Nethkin",
+    "7",
+    "student",
+    1
 );
