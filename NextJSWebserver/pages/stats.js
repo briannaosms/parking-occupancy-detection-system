788132e941c2b7d12bb43ce27d9b51844e888660
briannaosms <brianna.osms@protonmail.com>
2023-03-13 16:47:42 -0500

    changed index to backup, made index fit user story and created lots, stats, help with linking navigation

---
+++ NextJSWebserver/pages/stats.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function Lots() {
+  return (
+    <>
+      <h1>Parking Lot Statistics</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
