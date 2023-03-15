7f48d359de8ac10387209613cc2526463241f422
CoreyLBS13 <Coreyleebelk@icloud.com>
2023-03-15 16:51:24 -0500

    Adding text boxes

--- NextJSWebserver/pages/help.js
+++ NextJSWebserver/pages/help.js
@@ -4,6 +4,7 @@ export default function Lots() {
   return (
     <>
       <h1>Help</h1>
+      <textarea readOnly = {true} id="response" rows="10" cols="20" placeholder="Available Parking"></textarea>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
