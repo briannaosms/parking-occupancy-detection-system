bfcb7291744bcc07ace5a64b40be22b12df809e2
CoreyLBS13 <Coreyleebelk@icloud.com>
2023-03-20 16:02:28 -0500

    Added to help page

--- NextJSWebserver/pages/help.js
+++ NextJSWebserver/pages/help.js
@@ -4,7 +4,12 @@ export default function Lots() {
   return (
     <>
       <h1>Help</h1>
+      <h3>
       <textarea readOnly = {true} id="response" rows="10" cols="20" placeholder="Available Parking"></textarea>
+      </h3>
+      <h4>
+      <textarea readOnly = {true} id="response" rows="10" cols="20" placeholder="Lot Activity"></textarea>
+      </h4>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
