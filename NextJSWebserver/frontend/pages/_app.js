784538eef3c756a8acb9047dc8b524dfb39f18b2
landontomkins <landontomkins@gmail.com>
2023-03-23 11:12:29 -0500

    added new backend folder for api, made connection

--- NextJSWebserver/frontend/pages/_app.js
+++ NextJSWebserver/frontend/pages/_app.js
@@ -1,5 +1,24 @@
-import '@/styles/globals.css'
+import React, { useEffect, useState } from "react";
+import Axios from 'axios';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function App() {
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/get').then((response) =>{
+      console.log(response)
+      setBackendData(response.data)
+    })
+  }, [])
+  return (
+    <div>
+       {backendData.map((val) => {
+        return <h1>lot_name: {val.lot_name}</h1>
+      })}
+    </div>
+  )
 }
+
+export default App
