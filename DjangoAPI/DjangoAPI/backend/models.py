bb1e2ee9da49cdafbbecbef5e27cc4ae389ffad3
briannaosms <brianna.osms@protonmail.com>
2023-04-13 16:07:21 -0500

    reorganized + cleaned up the repo of unused software

--- DjangoAPI/DjangoAPI/backend/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Parkinglots(models.Model):
-    lot_name = models.CharField(max_length=255)
-    spaceID = models.CharField(max_length=255, primary_key=True)
-    type = models.CharField(max_length=255)
-    availability = models.SmallIntegerField()
